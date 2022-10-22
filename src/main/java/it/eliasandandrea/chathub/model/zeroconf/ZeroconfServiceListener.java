882cac4b33fe7e088b86c5c6cec3da63d0244a8c
Elias Binder <ebinder@unibz.it>
2022-10-22 23:21:27 +0200

    Fix Zeroconf - Calling the removeServerListener only once now

--- src/main/java/it/eliasandandrea/chathub/model/zeroconf/ZeroconfServiceListener.java
+++ src/main/java/it/eliasandandrea/chathub/model/zeroconf/ZeroconfServiceListener.java
@@ -1,22 +1,12 @@
 package it.eliasandandrea.chathub.model.zeroconf;
 
-import it.eliasandandrea.chathub.model.Server;
-import javafx.application.Platform;
-
 import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceListener;
-import java.util.Arrays;
 
 public class ZeroconfServiceListener implements ServiceListener {
 
-    private ServerAddedListener serverAddedListener;
-    private ServerRemovedListener serverRemovedListener;
 
-    public ZeroconfServiceListener() {
-        this.serverAddedListener = serverAddedListener;
-        this.serverRemovedListener = serverRemovedListener;
-
-    }
+    public ZeroconfServiceListener() {}
 
     @Override
     public void serviceAdded(ServiceEvent serviceEvent) {
