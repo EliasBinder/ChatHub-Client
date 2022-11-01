b7f9e7dff83e522988008b737683c31a15260e4f
Elias Binder <ebinder@unibz.it>
2022-11-01 12:06:38 +0100

    Communication now working!!!!!!

--- src/main/java/it/eliasandandrea/chathub/client/model/TCPClient.java
+++ src/main/java/it/eliasandandrea/chathub/client/model/TCPClient.java
@@ -7,11 +7,15 @@ import it.eliasandandrea.chathub.shared.crypto.Packet;
 import it.eliasandandrea.chathub.shared.protocol.ClientEvent;
 import it.eliasandandrea.chathub.shared.protocol.ServerEvent;
 import it.eliasandandrea.chathub.shared.protocol.clientEvents.HandshakeRequestEvent;
+import it.eliasandandrea.chathub.shared.protocol.serverEvents.HandshakeResponseEvent;
+import it.eliasandandrea.chathub.shared.util.ObjectByteConverter;
 import it.eliasandandrea.chathub.shared.util.SocketStreams;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.security.InvalidKeyException;
@@ -23,6 +27,8 @@ public class TCPClient {
 
     ServerEventCallback onServerEvent;
     private Socket socket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
     private CryptManager cryptManager;
     private PublicKey serverPublicKey;
 
@@ -35,7 +41,10 @@ public class TCPClient {
         Executors.newSingleThreadExecutor().submit(() -> {
             try {
                 socket = new Socket();
+                socket.setKeepAlive(true);
                 socket.connect(new InetSocketAddress(host, port), 7000);
+                inputStream = new DataInputStream(socket.getInputStream());
+                outputStream = new DataOutputStream(socket.getOutputStream());
 
                 onConnectionSuccess.run();
 
@@ -48,15 +57,16 @@ public class TCPClient {
                     }
                     final HandshakeRequestEvent handshakeRequestEvent = new HandshakeRequestEvent(cryptManager.getPublicKey());
                     final Packet packet = new Packet(handshakeRequestEvent);
-                    SocketStreams.writeObject(socket, packet);
+                    SocketStreams.writeObject(outputStream, packet);
                 });
-                boolean connected = true;
 
                 //Listen for incoming messages
+                boolean connected = true;
                 while (connected) {
                     try {
-                        EncryptedObjectPacket encryptedObjectPacket = (EncryptedObjectPacket) SocketStreams.readObject(socket);
-                        ServerEvent serverEvent = (ServerEvent) cryptManager.decryptToObject(encryptedObjectPacket);
+                        EncryptedObjectPacket encryptedObjectPacket = (EncryptedObjectPacket) SocketStreams.readObject(inputStream);
+                        Packet data = new Packet(cryptManager.decrypt(encryptedObjectPacket));
+                        ServerEvent serverEvent = (ServerEvent) data.getSerializable();
                         onMessage.onServerEvent(serverEvent);
                     } catch (Exception ex){
                         connected = false;
@@ -77,6 +87,6 @@ public class TCPClient {
     public void sendEvent(ClientEvent event) throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         EncryptedObjectPacket toSend = CryptManager.encrypt(event, serverPublicKey);
         if (socket != null)
-            Executors.newSingleThreadExecutor().submit(() -> SocketStreams.writeObject(socket, toSend));
+            Executors.newSingleThreadExecutor().submit(() -> SocketStreams.writeObject(outputStream, toSend));
     }
 }
