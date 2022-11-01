b82adbacd5e96fbd6303ea21cffac8e8378bdbcb
Elias Binder <ebinder@unibz.it>
2022-11-01 02:12:42 +0100

    Fixes and Improvements - Update implementation to use the logic from the submodule

--- src/main/java/it/eliasandandrea/chathub/client/model/TCPClient.java
+++ src/main/java/it/eliasandandrea/chathub/client/model/TCPClient.java
@@ -2,40 +2,62 @@ package it.eliasandandrea.chathub.client.model;
 
 import it.eliasandandrea.chathub.client.model.protocol.ServerEventCallback;
 import it.eliasandandrea.chathub.shared.crypto.CryptManager;
+import it.eliasandandrea.chathub.shared.crypto.EncryptedObjectPacket;
+import it.eliasandandrea.chathub.shared.crypto.Packet;
 import it.eliasandandrea.chathub.shared.protocol.ClientEvent;
+import it.eliasandandrea.chathub.shared.protocol.ServerEvent;
+import it.eliasandandrea.chathub.shared.protocol.clientEvents.HandshakeRequestEvent;
+import it.eliasandandrea.chathub.shared.util.SocketStreams;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 import java.util.concurrent.Executors;
 
 public class TCPClient {
 
     ServerEventCallback onServerEvent;
-    private CryptManager rsaCipher;
+    private Socket socket;
+    private CryptManager cryptManager;
+    private PublicKey serverPublicKey;
 
     public TCPClient(String host, int port, CryptManager cryptManager,
                      Runnable onConnectionFail, Runnable onConnectionInterrupted, Runnable onConnectionSuccess,
                      ServerEventCallback onMessage) {
         this.onServerEvent = onMessage;
-        this.rsaCipher = cryptManager;
+        this.cryptManager = cryptManager;
 
         Executors.newSingleThreadExecutor().submit(() -> {
             try {
-                Socket socket = new Socket();
+                socket = new Socket();
                 socket.connect(new InetSocketAddress(host, port), 7000);
+
+                onConnectionSuccess.run();
 
                 //initial handshake for submitting public key
                 Executors.newSingleThreadExecutor().submit(() -> {
-                    //TODO: send public key
+                    try {
+                        Thread.sleep(1000); //wait until client is ready to receive just to be sure
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    final HandshakeRequestEvent handshakeRequestEvent = new HandshakeRequestEvent(cryptManager.getPublicKey());
+                    final Packet packet = new Packet(handshakeRequestEvent);
+                    SocketStreams.writeObject(socket, packet);
                 });
-
-                onConnectionSuccess.run();
                 boolean connected = true;
 
                 //Listen for incoming messages
                 while (connected) {
                     try {
-                        //TODO: read message
+                        EncryptedObjectPacket encryptedObjectPacket = (EncryptedObjectPacket) SocketStreams.readObject(socket);
+                        ServerEvent serverEvent = (ServerEvent) cryptManager.decryptToObject(encryptedObjectPacket);
+                        onMessage.onServerEvent(serverEvent);
                     } catch (Exception ex){
                         connected = false;
                         onConnectionInterrupted.run();
@@ -52,7 +74,9 @@ public class TCPClient {
         this.onServerEvent = serverEventCallback;
     }
 
-    public void sendEvent(ClientEvent event) {
-        //TODO: send event
+    public void sendEvent(ClientEvent event) throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        EncryptedObjectPacket toSend = CryptManager.encrypt(event, serverPublicKey);
+        if (socket != null)
+            Executors.newSingleThreadExecutor().submit(() -> SocketStreams.writeObject(socket, toSend));
     }
 }
